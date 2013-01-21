2d842a611ebdcd83aa118023e52615460850c5f2
Michael Henderson <quoha@quoha.org>
2013-01-21 08:41:37 -0700

    Initial Commit

---
+++ derpOctoSansa/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  derpOctoSansa
+//
+//  Created by Michael Henderson on 1/21/13.
+//  Copyright (c) 2013 Michael Henderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
